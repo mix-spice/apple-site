851c45f0fa2e7484b527dfe9a39c89308041d250
coolware8 <coolware8@gmail.com>
2025-02-28 15:28:03 +0200

    Adding project files

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://86ca7703545bcdadbadd82e2424ec50d@o4508891459944448.ingest.de.sentry.io/4508891466367056",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+
+export default defineConfig({
+  base: "/Apple_Site/", // 👈 Replace with your repo name
+
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "southwave-llc",
+      project: "javascript-react",
+      include: "./dist", // Upload sourcemaps from the dist folder
+      authToken: process.env.SENTRY_AUTH_TOKEN, // Ensure it's set in CI/CD
+      release: process.env.npm_package_version, // Use package.json version
+      urlPrefix: "~/",
+    }),
+  ],
+
+  build: {
+    sourcemap: true, // Needed for Sentry error tracking
+  },
+});
