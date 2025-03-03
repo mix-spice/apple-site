3ad81bb3370a9f02dcaa85727ca61a2e48a5a7bf
coolware8 <coolware8@gmail.com>
2025-03-03 15:39:19 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 export default defineConfig({
-  base: "/Apple_Site/", // 👈 Replace with your repo name
+  base: "/apple-site/", // 👈 Replace with your repo name
 
   plugins: [
     react(),
